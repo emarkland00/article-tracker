bc401c5266e6c7fadbd9e73d0d087b81f2a77499
Errol Markland <ja23jr@gmail.com>
2015-06-30 22:31:12 -0400

    Added login method and a way to fetch upvoted articles.

--- hacker_news.py
+++ hacker_news.py
@@ -4,6 +4,7 @@ from config import ConfigClass
 from datetime import datetime, timedelta
 from mysql import Article
 import os.path
+from lxml import html
 
 class HackerNewsClientError(Exception):
     def __init__(self, value):
@@ -18,10 +19,22 @@ class HackerNewsClient:
         self.username = hn_config.username
         self.password = hn_config.password
         self.base_url = hn_config.base_url
+        self.user_agent = hn_config.user_agent
+        self.session = requests.Session()
         
     def login(self):
-        pass
-    
+        login_url = '{1}/login'.format(self.base_url) 
+        headers = { 'User-Agent': self.user_agent }
+        data = { 'goto': 'news', 'acct': self.username, 'pw': self.password }
+                
+        response = self.session.post(login_url,
+                            headers=headers,
+                            data=data)
+        # check if logged in
+        
     def fetch_upvoted_articles(self):
-        pass
+        url = '{0}/saved?id={1}'.format(self.base_url, self.username)
+        response = self.session.get(url)
+        tree = html.fromstring(response.text)
+        
     # maybe get reports for the last 30 DAYS?

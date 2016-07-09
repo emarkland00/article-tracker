52b6899da4dccfa8ca0d7ab8e8240bc73939fc15
Errol Markland <errol.markland@gmail.com>
2016-07-09 01:17:26 -0400

    Restructure the main.py file to accomodate config updates.

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
-from reddit import RedditClient
 from datetime import datetime
-from mysql import Article    
+from mysql import Article
+from config import ConfigClass
 
 def go():
+    if not ConfigClass().has_config():
+        print "Missing config.ini."
+        return
+
     fetch_reddit_stuff()
     fetch_hacker_news_stuff()
 
 def fetch_reddit_stuff():
+    from reddit import RedditClient
     print_msg("getting reddit stuff")
     client = RedditClient()
     filtered_posts = client.get_liked_posts().filter_by_new_listings().filter_by_subreddit(u'technology')
-        
+
     for post in filtered_posts:
         art = post.to_article()
         Article.create(name=art['name'], url=art['url'], source=art['source'], article_key=art['article_key'], timestamp=art['timestamp'])
-        
+
     print_msg("finished getting reddit stuff!")
 
 def fetch_hacker_news_stuff():
     """
     HACKER NEWS
-    
+
     https://news.ycombinator.com/saved?id=sol7117
-    
+
     requires user session cookie...
     """
     pass
+    
 
 def print_msg(msg):
     date = datetime.now()

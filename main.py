32f1608c2419a308998ba8acd6d0fe09f7319314
Errol Markland <errol.markland@gmail.com>
2016-07-30 17:23:14 -0400

    Create more generic filter method and generic insert method.

--- main.py
+++ main.py
@@ -21,24 +21,42 @@ def fetch_reddit_stuff():
         print "Failed to load up reddit client"
         return
 
-    filtered_posts = client.get_liked_posts().filter_by_new_listings().filter_by_subreddit(u'technology',u'BlackPeopleTwitter').posts()
+    filtered_posts = client.get_liked_posts().filter_by_subreddit(u'technology',u'BlackPeopleTwitter').posts()
 
+    articles = []
     for post in filtered_posts:
-        art = post.to_article()
-        create_article(art['name'], art['url'], 'reddit', art['article_key'], art['timestamp'])
+        art = post.as_json()
+        articles.append(create_article(art['name'], art['url'], 'reddit', art['article_key'], art['timestamp']))
 
+    save_articles(articles)
     print_msg("finished getting reddit stuff!")
 
 def fetch_hacker_news_stuff():
     from hackernews import HackerNewsClient as hn
     client = hn.HackerNewsClient()
-    articles = client.fetch_upvoted_articles()
-    for a in articles:
+    posts = client.fetch_upvoted_posts()
+    articles = [
         create_article(a['title'], a['link'], 'hacker news', a['id'], a['timestamp'])
-    import pdb; pdb.set_trace()
+        for a
+        in posts
+    ]
+    articles = filter_by_new_listings(articles)
+    save_articles(articles)
+    print_msg("finished getting hacker news stuff!")
+
+def filter_by_new_listings(articles):
+    source = articles[0].source
+    ids = [ a.article_key for a in articles ]
+    existing = Article.find_all_by_source_and_ids(source, ids)
+    known = [ a.article_key for a in existing ]
+    return [ a for a in articles if a.article_key not in known ]
+
+def save_articles(articles):
+    if articles:
+        Article.bulk_insert(articles)
 
 def create_article(name, url, source, article_key, timestamp):
-    return Article.create(
+    return Article(
         name=name,
         url=url,
         source=source,

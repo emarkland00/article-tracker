2ff27ca17867efb632fc2d79834b9f533707cb2d
Errol Markland <errol.markland@gmail.com>
2016-10-19 02:08:54 -0400

    Cleaning up the code to handle connection state internally

--- mysql.py
+++ mysql.py
@@ -8,6 +8,14 @@ class MySQLModel(Model):
     class Meta:
         database = __MYSQL_DB__
 
+    @classmethod
+    def has_db_configured(cls):
+        """
+        Checks whether if the db has been configured
+        """
+        import pdb; pdb.set_trace()
+        return True;
+
 class Article(MySQLModel):
     """A generic container for holding information"""
     article_id = PrimaryKeyField()
@@ -19,13 +27,20 @@ class Article(MySQLModel):
 
     @classmethod
     def find_all_by_source_and_ids(cls, source, ids):
+        if not super(Article).has_db_configured():
+            return []
+
         if not source and ids:
             return []
-        import pdb; pdb.set_trace()
+
         return [ a for a in Article.select().where((Article.article_key << ids) & (Article.source == source)) ]
 
     @classmethod
     def bulk_insert(cls, articles):
+        if not super(Article).has_db_configured():
+            return
+
+        # add logic to check if instance has been set
         arts = [ {
             "name": a.name,
             "url": a.url,
@@ -36,20 +51,21 @@ class Article(MySQLModel):
         with __MYSQL_DB__.atomic():
             Article.insert_many(arts).execute()
 
-# Check that we have the details needed to connect to database
-mysql_config = ConfigClass().get_mysql_config()
-if not mysql_config:
-    print "Config file missing section for mysql"
-else:    
-    import pdb; pdb.set_trace()
-    # initialize database connection
-    __MYSQL_DB__.init(
-        mysql_config['db_name'],
-        host=mysql_config['host'],
-        user=mysql_config['username'],
-        passwd=mysql_config['password'])
+def init():
+    global __MYSQL_DB__
 
-    # Auto generate tables needed for this operation (if they don't exist)
-    if not Article.table_exists():
-        Article.create_table()
+    # Check that we have the details needed to connect to database
+    mysql_config = ConfigClass().get_mysql_config()
+    if not mysql_config:
+        print "Config file missing section for [mysql]"
+    else:
+        # initialize database connection
+        __MYSQL_DB__.init(
+            mysql_config['db_name'],
+            host=mysql_config['host'],
+            user=mysql_config['username'],
+            passwd=mysql_config['password'])
 
+        # Auto generate tables needed for this operation (if they don't exist)
+        if not Article.table_exists():
+            Article.create_table()
